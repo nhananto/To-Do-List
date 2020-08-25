7b1d505bb00e0bdbbc17df240772bf02d89e3c1c
Nazmul Hossain Ananto <nazmul15-10642@diu.edu.bd>
2020-08-26 03:17:45 +0600

    Updated Static
    External Static file is added

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from . models import *
 from . forms import *
 # Create your views here.
 
+
 def index(request):
     tasks = Task.objects.all()
     form = TaskForm()
@@ -12,8 +13,9 @@ def index(request):
         if form.is_valid():
             form.save()
         return redirect('/')
-    context = {'tasks' :tasks, 'form' :form}
-    return render(request, 'tasks/list.html',context)
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
+
 
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
@@ -29,10 +31,11 @@ def updateTask(request, pk):
     context = {'form': form}
     return render(request, 'tasks/update_task.html', context)
 
+
 def deleteTask(request, pk):
     item = Task.objects.get(id=pk)
     if request.method == 'POST':
         item.delete()
         return redirect('/')
-    context = {'item' :item}
+    context = {'item': item}
     return render(request, 'tasks/delete.html', context)
